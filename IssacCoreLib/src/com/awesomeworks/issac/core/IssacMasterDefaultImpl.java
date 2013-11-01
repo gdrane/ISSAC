1c25730fef27964df9e6fbe38b6e74918be88e9a
Gauresh <rane.gauresh@gmail.com>
2013-10-31 23:19:40 -0700

    Add a Default impl for master service

---
+++ IssacCoreLib/src/com/awesomeworks/issac/core/IssacMasterDefaultImpl.java
@@ -0,0 +1,14 @@
+package com.awesomeworks.issac.core;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.BusObject;
+
+public class IssacMasterDefaultImpl implements IssacMasterInterface,BusObject {
+
+	@Override
+	public String Search(String inStr) throws BusException {
+		
+		return null;
+	}
+
+}
